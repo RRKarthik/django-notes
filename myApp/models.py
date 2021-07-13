5bc57734db1e7c577bdcd49174c1327282e9afb0
Devalapalli Tejeshwar Reddy <tejeshwarreddydevalapalli@gmail.com>
2021-07-13 19:51:08 +0530

    Added user specific functionalities

--- myApp/models.py
+++ myApp/models.py
@@ -5,3 +5,7 @@ class Notes(models.Model):
 	title = models.CharField(max_length=50)
 	date = models.DateField(blank=True,null=True)
 	body = models.CharField(max_length=250)
+	reqid = models.IntegerField(blank=True,null=True)
+
+	def __str__(self):
+		return self.title
