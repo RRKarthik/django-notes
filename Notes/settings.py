9811444440cf26f85cfc0299891ad08733b3f65f
epicman25 <67092102+DevalapalliTejeshwarReddy@users.noreply.github.com>
2021-11-02 09:00:57 +0530

    Update settings.py

--- Notes/settings.py
+++ Notes/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1l^^ny2b%p)1@&wsehpx*s^&vi&keiqk7rn!nkx289y!o2fq3@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
